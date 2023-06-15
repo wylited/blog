448e35dcab3bc2e3cd6b600b7dbbecf59f548ebc
wylited <44154103+wylited@users.noreply.github.com>
2023-06-15 22:44:43 +0800

    blog+ exms

---
+++ pages/2023/exms.vue
@@ -0,0 +1,60 @@
+<template>
+<Blog>
+
+<p>
+exms -&gt; exam stress
+</p>
+
+<p>
+communal degredation through exam stress
+</p>
+
+<p>
+Lately, I've been completing alot of exams for iGCSEs (international general certificate of education).
+</p>
+
+<p>
+Everyone in my year does these exams and I've done alot of them, 22 currently with only two left ay. And although I should be studying for triple physics, I'd rather bring light to a rather negative phenomenom that tends to happen in larger student bodies during exam seasons.
+</p>
+
+<p>
+Stress, noun
+</p>
+<ul class="org-ul">
+<li>a state of mental or emotional strain or tension resulting from adverse or demanding circumstances.
+(google definitions)</li>
+</ul>
+
+<p>
+Exams certainly present adverse and demanding circumstances. Your perfomance gets measured onto a linear scale for each subject. Exams require special preperation to do well in and even though I do believe that exams are flawed in its nature, examination boards aren't going away any time soon so we may as well learn to adapt to it. (that may be the topic of another post)
+</p>
+
+<p>
+Exams bring out stress and anxiety in students as it determines how well you are doing. This pressure of the stress can also be from a secondary figure such as parents or other peers to do better.
+</p>
+
+<p>
+Most humans do not perform well in stressful scenarios. You cannot expect a teenager with barely 15-16 years of experience in this world to be able to manage every anxiety inducing situation with a cool and calm manner.
+</p>
+
+<p>
+The major problem isn't individual stress, as much as it degrades ones own mentality, it really comes into play when there is communal stress. the communal stress tends to happen when an majority of the student body are feeling even midly pressured from exams due to inadequate preparement or as mentioned earlier pressure from secondary figures.
+</p>
+
+<p>
+Communal stress especially causes students who aren't pressured at all and are ready for their exams to start believing that they aren't as prepared as they should be, which should not be the case. Stress is not the best the response to pressure obviously and so communal stress can degrade an overwhelming number of the students ability to perform well during a test.
+</p>
+
+<p>
+This slowly snowballs into a cycle that doesn't stop easily. You can only choose to remove yourself from it and focus on working to solve the pressure instead of wasting your time stressing about.
+</p>
+
+<p>
+peace out
+</p>
+<ul class="org-ul">
+<li>wyli</li>
+</ul>
+
+</Blog>
+</template>
--- pages/2023/index.vue
+++ pages/2023/index.vue
@@ -5,9 +5,10 @@
         { name: '2023', link: '/2023' }
     ]">
     <h1 class="text-3xl text-primary py-6 underline">2023</h1>
-    <ul class="py-5">
+    <ul class="py-5 text-left">
       <li>
-        <NuxtLink to="2023/dlt" class="text-accent">dlt</NuxtLink>
+        <NuxtLink to="2023/dlt" class="text-accent ml-1.5">dlt</NuxtLink>
+        <NuxtLink to="2023/exms" class="text-accent ml-2.5">exms</NuxtLink>
       </li>
     </ul>
   </Hero>
