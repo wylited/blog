41275d0a790596209597c46015ebd1552b5c33c3
wylited <44154103+wylited@users.noreply.github.com>
2023-10-08 21:05:06 +0800

    update margins

--- pages/2023/index.vue
+++ pages/2023/index.vue
@@ -8,10 +8,10 @@
     <ul class="py-5 text-left">
       <li>
 
-        <NuxtLink to="2023/sts" class="text-accent ml-1.5">sts</NuxtLink>
-        <NuxtLink to="2023/dlt" class="text-accent ml-1.5">dlt</NuxtLink>
+        <NuxtLink to="2023/sts" class="text-accent ml-3.5">sts</NuxtLink>
+        <NuxtLink to="2023/dlt" class="text-accent ml-2.5">dlt</NuxtLink>
         <NuxtLink to="2023/exms" class="text-accent ml-2.5">exms</NuxtLink>
-        <NuxtLink to="2023/prse" class="text-accent ml-1.5">prse</NuxtLink>
+        <NuxtLink to="2023/prse" class="text-accent ml-2.5">prse</NuxtLink>
       </li>
     </ul>
   </Hero>
