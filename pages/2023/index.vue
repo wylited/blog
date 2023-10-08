d77327a7f54fab86a590cd6edf9c476009c2da0f
wylited <44154103+wylited@users.noreply.github.com>
2023-10-08 21:03:18 +0800

    new prose and links

--- pages/2023/index.vue
+++ pages/2023/index.vue
@@ -7,10 +7,12 @@
     <h1 class="text-3xl text-primary py-6 underline">2023</h1>
     <ul class="py-5 text-left">
       <li>
+
+        <NuxtLink to="2023/sts" class="text-accent ml-1.5">sts</NuxtLink>
         <NuxtLink to="2023/dlt" class="text-accent ml-1.5">dlt</NuxtLink>
         <NuxtLink to="2023/exms" class="text-accent ml-2.5">exms</NuxtLink>
+        <NuxtLink to="2023/prse" class="text-accent ml-1.5">prse</NuxtLink>
       </li>
     </ul>
   </Hero>
 </template>
-  
---
+++ pages/2023/prse.vue
@@ -0,0 +1,111 @@
+<template>
+<Blog>
+
+<p>
+prse -&gt; prose
+</p>
+
+<p>
+a collection of short stuff I wrote, not long enough to be anything real.
+</p>
+
+<hr />
+
+<p>
+persistent weed, in the crack,
+shallow pool, reflecting attacks.
+spalling concrete, resilient shelter
+but you wont buckle, under the pressure
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+ways to lose memories:
+you forget which key, you use for the safe
+you forget the safe number, and you cant test your key with every safe
+you give the safe to someone else, and they run away with it
+you lock the safe, within several other safes, and throw away all the keys
+you throw the safe further into the future, for a later you to deal with
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+things that remind me of you &amp; the small things you do
+is it
+standing in the rain, holding a black umbrella
+that's alike to texting you, from the comfort of my bed.
+or was it
+staring out of the window, into the darkness of the storm.
+the soft words you speak, wrapping my head.
+the touch of your cold hands, that you use to write
+extravagant fortresses of sweet, sweet rambling.
+the touch of cold stone under my neck, the blossom of blood spreading from it.
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-26 Wed&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+you remind me
+of a ghost, That i dont get to see.
+appearing every todays, whispering in my ear.
+she sacrified her face, for no fortune nor fame.
+a deal made with someone worse than the devil.
+a deal made with her irrationally driven self.
+for someone who thinks and breathes in logic,
+I wonder how one could be so stupid.
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-26 Wed&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+I dont get to see
+pretty clouds often enough
+but fortunately
+i can see you
+every day and night
+somewhere only we know
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-09-23 Sat&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+when you speak of warmth, you feel it, then you lose it.
+so why do I speak of warmth when I'm hot headed?
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-30 Sun&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+I think i need some sedative
+falling victim to the scheduled
+it all feels too repetitive
+"go buy some milk again"
+rise before the sun, work my stress away
+try to make the most of the next hour
+living life on the daytoday
+never gonna falter, but let me sway
+from side to side
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-05-14 Sun&gt;</span></span>
+</p>
+
+<hr />
+
+<p>
+leave me be
+for i am too tired to argue
+and if i do argue
+you will end up tireder than me
+for that is all i can do
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
+</p>
+
+</Blog>
+</template>
