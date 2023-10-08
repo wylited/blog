1fd66482a17906a9db7502752b9b65f5a6e3779e
wylited <44154103+wylited@users.noreply.github.com>
2023-10-08 21:08:06 +0800

    update prose

--- pages/2023/prse.vue
+++ pages/2023/prse.vue
@@ -6,16 +6,31 @@ prse -&gt; prose
 </p>
 
 <p>
-a collection of short stuff I wrote, not long enough to be anything real.
+a collection of short stuff I wrote, not long enough to be anything of substance or value.
 </p>
+
+
+
 
 <hr />
 
 <p>
 persistent weed, in the crack,
+</p>
+
+<p>
 shallow pool, reflecting attacks.
+</p>
+
+<p>
 spalling concrete, resilient shelter
+</p>
+
+<p>
 but you wont buckle, under the pressure
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
 </p>
 
@@ -23,41 +38,115 @@ but you wont buckle, under the pressure
 
 <p>
 ways to lose memories:
+</p>
+
+<p>
 you forget which key, you use for the safe
+</p>
+
+<p>
 you forget the safe number, and you cant test your key with every safe
+</p>
+
+<p>
 you give the safe to someone else, and they run away with it
+</p>
+
+<p>
 you lock the safe, within several other safes, and throw away all the keys
+</p>
+
+<p>
 you throw the safe further into the future, for a later you to deal with
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
 </p>
+
 
 <hr />
 
 <p>
 things that remind me of you &amp; the small things you do
+</p>
+
+<p>
 is it
+</p>
+
+<p>
 standing in the rain, holding a black umbrella
+</p>
+
+<p>
 that's alike to texting you, from the comfort of my bed.
+</p>
+
+<p>
 or was it
+</p>
+
+<p>
 staring out of the window, into the darkness of the storm.
+</p>
+
+<p>
 the soft words you speak, wrapping my head.
+</p>
+
+<p>
 the touch of your cold hands, that you use to write
+</p>
+
+<p>
 extravagant fortresses of sweet, sweet rambling.
+</p>
+
+<p>
 the touch of cold stone under my neck, the blossom of blood spreading from it.
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-26 Wed&gt;</span></span>
 </p>
+
 
 <hr />
 
 <p>
 you remind me
+</p>
+
+<p>
 of a ghost, That i dont get to see.
+</p>
+
+<p>
 appearing every todays, whispering in my ear.
+</p>
+
+<p>
 she sacrified her face, for no fortune nor fame.
+</p>
+
+<p>
 a deal made with someone worse than the devil.
+</p>
+
+<p>
 a deal made with her irrationally driven self.
+</p>
+
+<p>
 for someone who thinks and breathes in logic,
+</p>
+
+<p>
 I wonder how one could be so stupid.
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-26 Wed&gt;</span></span>
 </p>
 
@@ -65,19 +154,45 @@ I wonder how one could be so stupid.
 
 <p>
 I dont get to see
+</p>
+
+<p>
 pretty clouds often enough
+</p>
+
+<p>
 but fortunately
+</p>
+
+<p>
 i can see you
+</p>
+
+
+<p>
 every day and night
+</p>
+
+<p>
 somewhere only we know
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-09-23 Sat&gt;</span></span>
 </p>
+
 
 <hr />
 
 <p>
-when you speak of warmth, you feel it, then you lose it.
+when you speak of warmth, you make me feel it , but then you end up losing it.
+</p>
+
+<p>
 so why do I speak of warmth when I'm hot headed?
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-07-30 Sun&gt;</span></span>
 </p>
 
@@ -85,14 +200,41 @@ so why do I speak of warmth when I'm hot headed?
 
 <p>
 I think i need some sedative
+</p>
+
+<p>
 falling victim to the scheduled
+</p>
+
+<p>
 it all feels too repetitive
+</p>
+
+<p>
 "go buy some milk again"
+</p>
+
+<p>
 rise before the sun, work my stress away
+</p>
+
+<p>
 try to make the most of the next hour
+</p>
+
+<p>
 living life on the daytoday
+</p>
+
+<p>
 never gonna falter, but let me sway
+</p>
+
+<p>
 from side to side
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-05-14 Sun&gt;</span></span>
 </p>
 
@@ -100,12 +242,28 @@ from side to side
 
 <p>
 leave me be
+</p>
+
+<p>
 for i am too tired to argue
+</p>
+
+<p>
 and if i do argue
+</p>
+
+<p>
 you will end up tireder than me
+</p>
+
+<p>
 for that is all i can do
+</p>
+
+<p>
 <span class="timestamp-wrapper"><span class="timestamp">&lt;2023-10-08 Sun&gt;</span></span>
 </p>
+
 
 </Blog>
 </template>
