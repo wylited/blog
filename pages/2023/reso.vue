ec0971ff8d327b5f943e427c970a205f47858c21
wylited <44154103+wylited@users.noreply.github.com>
2023-12-19 22:28:26 +0800

    wrote the reso blog post

--- pages/2023/reso.vue
+++ pages/2023/reso.vue
@@ -17,7 +17,49 @@ So, I did write resolutions last year, I can read it write now.
 its really not that accurate though. I did manage to do some of the stuff, but I think the real issue was in the resolutions.
 </p>
 
+<p>
+Now that I think back over them, they were alot of things that I wanted to achieve in the moment.
+</p>
 
+<p>
+It really wasn't something that I thought over a long period of time, I really didn't understand the concept of a year even that well.
+</p>
+
+<p>
+Now thats not to say I haven't accomplished anything this year, I am really proud on what I've managed to complete.
+</p>
+
+<p>
+I'm just a bit scared that I won't become to attached to the winning or the goal side of things.
+</p>
+
+<p>
+Enjoying the journey is really fun and managing to keep the motivation for it is the hardest part.
+</p>
+
+<p>
+It's even more interesting to how I believe I could have done alot more this year.
+</p>
+
+<p>
+But I think that just implies of how much more I can accomplish in the following years.
+</p>
+
+<p>
+I wonder if I will write new years resolutions this year, however, I think if you want to get things done, don't wait for the new years to start? you know. (check out ruff's youtube videos if you have the time, he deserves the praise and is really good).
+</p>
+
+<p>
+Just do it now. The haters don't know us.
+</p>
+
+<p>
+only the real mf's read this blog.
+</p>
+
+<p>
+peace out, love you.
+</p>
 
 </Blog>
 </template>
